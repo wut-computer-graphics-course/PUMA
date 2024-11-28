12f37e95be4daf24014093df261168767db242c3
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2024-11-28 16:01:00 +0100

    core: add children layers to Layer

--- src/SymBase/Core/Layers/Layer.hh
+++ src/SymBase/Core/Layers/Layer.hh
@@ -12,7 +12,13 @@ namespace sym_base
     Layer() {}
 
     /// @brief Virtual destructor.
-    virtual ~Layer() {}
+    virtual ~Layer()
+    {
+      for (auto& child : m_children)
+      {
+        delete child;
+      }
+    }
 
     /// @brief Enables layer.
     virtual void attach() {}
@@ -20,14 +26,32 @@ namespace sym_base
     virtual void detach() {}
     /// @brief Runs layer logic.
     /// @param dt Time that has passed since last update.
-    virtual void update(float dt) {}
+    virtual void update(float dt)
+    {
+      for (auto child : m_children)
+      {
+        child->update(dt);
+      }
+    }
     /// @brief Runs layer logic when rendering imgui.
     /// @param dt Time that has passed since last update.
-    virtual void imgui_update(float dt) {}
+    virtual void imgui_update(float dt)
+    {
+      for (auto child : m_children)
+      {
+        child->imgui_update(dt);
+      }
+    }
     /// @brief Handle event by this layer.
     /// @param event Event to be handled.
     /// @param dt Time that has passed since last update.
     virtual void handle_event(Event& event, float dt) {}
+
+   protected:
+    virtual void push_child_layer(Layer* layer) { m_children.emplace_back(layer); }
+
+   protected:
+    std::vector<Layer*> m_children;
   };
 } // namespace sym_base
 
