fe8fe2c33b6606d3b83aa63885d73213ecec3246
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2024-12-05 14:23:03 +0100

    maintenance: move to SymBase git submodule

--- src/SymBase/Core/Events/Events.hh
+++
@@ -1,8 +0,0 @@
-#ifndef SYM_BASE_EVENTS_HH
-#define SYM_BASE_EVENTS_HH
-
-#include "KeyEvent.hh"
-#include "MouseEvent.hh"
-#include "WindowEvent.hh"
-
-#endif // SYM_BASE_EVENTS_HH
--- src/SymBase/Core/Renderer/Camera/Camera.cc
+++
@@ -1,9 +0,0 @@
-#include "Camera.hh"
-
-namespace sym_base
-{
-  void Camera::set_perspective(float fov, float aspect_ratio, float near, float far)
-  {
-    m_projection_mat = glm::perspective(fov, aspect_ratio, near, far);
-  }
-} // namespace sym_base
