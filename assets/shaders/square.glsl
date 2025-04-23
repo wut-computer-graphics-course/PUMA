33962eaa131e93d7a9e5441a29c5c9b35f7ff457
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2025-04-23 16:45:14 +0200

    sandbox: instancing

--- assets/shaders/square.glsl
+++ assets/shaders/square.glsl
@@ -2,8 +2,8 @@
 #version 450
 
 layout (location = 0) in vec3 a_Position;
-layout (location = 1) in vec3 a_Color;
-layout (location = 2) in vec2 a_TexCoord;
+layout (location = 1) in vec2 a_TexCoord;
+layout (location = 2) in vec3 i_Offset;
 
 uniform mat4 u_MVP;
 
@@ -11,7 +11,7 @@ out vec2 v_TexCoord;
 
 void main()
 {
-    gl_Position = u_MVP * vec4(a_Position, 1.0);
+    gl_Position = u_MVP * vec4(a_Position + i_Offset, 1.0);
 
     v_TexCoord = a_TexCoord;
 }
