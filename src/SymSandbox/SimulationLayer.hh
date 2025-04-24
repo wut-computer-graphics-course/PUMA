b551346a94de9193a7bfac2cdbb79e1f633b0b1a
Mikolaj Kida <mikolaj.kida@skyengine.ai>
2025-04-24 21:40:21 +0200

    refactor: removed commented code.

--- src/SymSandbox/SimulationLayer.hh
+++ src/SymSandbox/SimulationLayer.hh
@@ -24,30 +24,6 @@ namespace sym
       auto camera = Renderer::get_camera();
 
       Renderer::submit(m_pumaModel1);
-      // cube
-      // {
-      //   m_cube.m_shader->bind();
-      //   m_cube.m_shader->upload_uniform_float3("u_Color", m_cube.m_color);
-      //   m_cube.m_rotation *= glm::angleAxis(dt / 2, glm::vec3(1, 1, 0));
-      //   auto mvp = camera->get_projection() * camera->get_view() * m_cube.get_model_mat();
-      //   m_cube.m_shader->upload_uniform_mat4("u_MVP", mvp);
-      //   RenderCommand::set_draw_primitive(DrawPrimitive::LINES);
-      //   RenderCommand::set_line_width(2);
-      //   Renderer::submit(*m_cube.m_shader, *m_cube.m_va, m_cube.get_model_mat());
-      //   m_cube.m_va->unbind();
-      // }
-      // // square
-      // {
-      //   m_square.m_shader->bind();
-      //   auto mvp = camera->get_projection() * camera->get_view();
-      //   m_square.m_shader->upload_uniform_mat4("u_MVP", mvp);
-      //   m_square.m_texture->bind(0);
-      //   m_square.m_shader->upload_uniform_int("u_Texture", 0);
-      //   RenderCommand::set_draw_primitive(DrawPrimitive::TRIANGLES);
-      //   RenderCommand::set_line_width(1);
-      //   Renderer::submit(*m_square.m_shader, *m_square.m_va, glm::mat4(1));
-      //   m_square.m_va->unbind();
-      // }
     }
 
     virtual void imgui_update(float dt)
