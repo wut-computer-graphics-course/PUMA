53f2b9909757b32c17300e5f60eb4bc78b56a0b0
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2025-04-25 15:29:34 +0200

    sandbox: use renderer API in shadow volume example

--- src/SymSandbox/ShadowVolumeExampleLayer.hh
+++ src/SymSandbox/ShadowVolumeExampleLayer.hh
@@ -23,11 +23,9 @@ namespace sym
         m_shadow_volume_shader = ShaderSystem::acquire("shaders/shadow_volume.glsl");
       }
 
-      const float wall_size = 5;
-
       // walls
       {
-        auto&& [vertices, indices] = generate_cuboid({ wall_size, wall_size, wall_size }, { 1, 0, 0 });
+        auto&& [vertices, indices] = generate_cuboid({ m_walls.m_size, m_walls.m_size, m_walls.m_size }, { 1, 1, 1 });
         for (auto& v : vertices)
         {
           v.m_normal *= -1;
@@ -36,7 +34,7 @@ namespace sym
         m_walls.m_model     = std::make_shared<Model>(vertices,
                                                   indices,
                                                   ModelParams{ .m_position = true, .m_color = true, .m_normal = true });
-        m_walls.m_model_mat = glm::translate(glm::mat4(1), glm::vec3(0, wall_size / 2, 0));
+        m_walls.m_model_mat = glm::translate(glm::mat4(1), glm::vec3(0, m_walls.m_size / 2, 0));
       }
 
       // light
@@ -44,9 +42,10 @@ namespace sym
         const glm::vec3 size       = { .2f, .2f, .2f };
         auto&& [vertices, indices] = generate_cuboid(size, { 1, 1, 1 });
 
-        m_light.m_model = std::make_shared<Model>(vertices, indices, ModelParams{ .m_position = true });
-        m_light.m_model_mat =
-            glm::translate(glm::mat4(1), glm::vec3(wall_size / 2 - size.x, wall_size - size.y, wall_size / 2 - size.z));
+        m_light.m_model     = std::make_shared<Model>(vertices, indices, ModelParams{ .m_position = true });
+        m_light.m_model_mat = glm::translate(
+            glm::mat4(1),
+            glm::vec3(m_walls.m_size / 2 - size.x, m_walls.m_size - size.y, m_walls.m_size / 2 - size.z));
         m_light.m_color = glm::vec3(1, 1, 1);
       }
 
@@ -79,44 +78,56 @@ namespace sym
 
     void update(float dt) override
     {
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
+      // -------------------------------------------------
+      // Ambient pass to make sure z-buffer contains data
+      // -------------------------------------------------
       draw_walls(m_ambient_shader);
       draw_boxes(m_ambient_shader);
       draw_lights();
+      // -------------------------------------------------
 
-      glEnable(GL_STENCIL_TEST);
-      glEnable(GL_DEPTH_TEST);
-
-      glStencilFunc(GL_ALWAYS, 0, 0xFF);
-      glEnable(GL_DEPTH_CLAMP);
-
-      glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
-      glDepthMask(GL_FALSE);
-
-      glStencilOpSeparate(GL_BACK, GL_KEEP, GL_INCR_WRAP, GL_KEEP);
-      glStencilOpSeparate(GL_FRONT, GL_KEEP, GL_DECR_WRAP, GL_KEEP);
+      // -------------------------------------------------------------------
+      // Create shadow volumes of objects and render into the stencil buffer
+      // -------------------------------------------------------------------
+      RenderCommand::set_color_mask(false, false, false, false);
+      RenderCommand::stencil_test(true);
+      RenderCommand::depth_test(true);
+      RenderCommand::set_stencil_func(CompFunc::ALWAYS, 0, 0xFF);
+      RenderCommand::depth_clamp(true);
+      RenderCommand::depth_mask(false);
+      RenderCommand::set_stencil_op_per_face(Face::BACK, StencilAct::KEEP, StencilAct::INCR_WRAP, StencilAct::KEEP);
+      RenderCommand::set_stencil_op_per_face(Face::FRONT, StencilAct::KEEP, StencilAct::DECR_WRAP, StencilAct::KEEP);
       draw_boxes(m_shadow_volume_shader);
+      RenderCommand::depth_clamp(false);
+      // -------------------------------------------------------------------
 
-      glDisable(GL_DEPTH_CLAMP);
-
+      // ------------------------------------------------------------------------------------
+      // Render the scene again, using lightning and the stencil buffer as a mask for shadows
+      // ------------------------------------------------------------------------------------
       RenderCommand::set_color_mask(true, true, true, true);
-
-      glDepthFunc(GL_EQUAL);
-
-      glStencilFunc(GL_EQUAL, 0x0, 0xFF);
-      glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-
+      RenderCommand::set_depth_func(CompFunc::EQUAL);
+      RenderCommand::set_stencil_func(CompFunc::EQUAL, 0x0, 0xFF);
+      RenderCommand::set_stencil_op(StencilAct::KEEP, StencilAct::KEEP, StencilAct::KEEP);
       draw_walls(m_phong_shader);
       draw_boxes(m_phong_shader);
       draw_lights();
+      RenderCommand::depth_mask(true);
+      RenderCommand::set_depth_func(CompFunc::LEQUAL);
+      RenderCommand::stencil_test(false);
+      // ------------------------------------------------------------------------------------
 
-      glDepthMask(GL_TRUE);
-      glDepthFunc(GL_LEQUAL);
-
-      glDisable(GL_STENCIL_TEST);
-
+      // ------------
+      // Update scene
+      // ------------
+      static bool first_time = true;
+      if (first_time)
+      {
+        auto camera = dynamic_cast<OrbitCamera*>(Renderer::get_camera());
+        if (camera) { camera->look_at({ 0, m_walls.m_size / 2, 0 }); }
+        first_time = false;
+      }
       m_box2.m_angle += dt / 2;
+      // ------------
     }
 
     virtual void imgui_update(float dt) {}
@@ -195,6 +206,7 @@ namespace sym
     {
       std::shared_ptr<Model> m_model;
       glm::mat4 m_model_mat;
+      const float m_size = 5;
     } m_walls;
 
     struct
