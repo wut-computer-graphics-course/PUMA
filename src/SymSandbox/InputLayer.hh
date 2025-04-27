c2d86288aff25f529a161502a20402c62853e94e
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2025-04-27 12:05:18 +0200

    sandbox: replace orbit camera with fps camera

--- src/SymSandbox/InputLayer.hh
+++ src/SymSandbox/InputLayer.hh
@@ -15,25 +15,41 @@ namespace sym
 
     void update(float dt) { handle_keyboard_input(dt); }
 
+    void handle_event(Event& e, float dt) override
+    {
+      EventDispatcher dispatcher(e);
+      dispatcher.dispatch<MouseMovedEvent>(BIND_EVENT_FOR_FUN(InputLayer::mouse_moved_event_handler, dt));
+    }
+
    private:
     void handle_keyboard_input(float dt)
     {
-      float zoom         = 0;
-      glm::vec2 rotation = { 0, 0 };
-      if (Input::is_key_pressed(GLFW_KEY_W)) { rotation.y -= m_keyboard_sens; }
-      if (Input::is_key_pressed(GLFW_KEY_S)) { rotation.y += m_keyboard_sens; }
-      if (Input::is_key_pressed(GLFW_KEY_A)) { rotation.x -= m_keyboard_sens; }
-      if (Input::is_key_pressed(GLFW_KEY_D)) { rotation.x += m_keyboard_sens; }
-      if (Input::is_key_pressed(GLFW_KEY_Q)) { zoom += m_keyboard_sens; }
-      if (Input::is_key_pressed(GLFW_KEY_E)) { zoom -= m_keyboard_sens; }
+      auto camera = static_cast<FpsCamera*>(Renderer::get_camera());
+      if (Input::is_key_pressed(GLFW_KEY_W)) { camera->move(FORWARD, m_keyboard_sens * dt); }
+      if (Input::is_key_pressed(GLFW_KEY_S)) { camera->move(BACKWARD, m_keyboard_sens * dt); }
+      if (Input::is_key_pressed(GLFW_KEY_A)) { camera->move(LEFT, m_keyboard_sens * dt); }
+      if (Input::is_key_pressed(GLFW_KEY_D)) { camera->move(RIGHT, m_keyboard_sens * dt); }
+      if (Input::is_key_pressed(GLFW_KEY_E)) { camera->move(UP, m_keyboard_sens * dt); }
+      if (Input::is_key_pressed(GLFW_KEY_Q)) { camera->move(DOWN, m_keyboard_sens * dt); }
+    }
 
-      auto camera = dynamic_cast<OrbitCamera*>(Renderer::get_camera());
-      camera->zoom(zoom, dt);
-      camera->rotate(rotation.x, rotation.y, dt);
+    bool mouse_moved_event_handler(sym_base::MouseMovedEvent& e, float dt)
+    {
+      auto camera        = static_cast<FpsCamera*>(Renderer::get_camera());
+      bool input_handled = false;
+
+      if (Input::is_mouse_button_pressed(GLFW_MOUSE_BUTTON_LEFT))
+      {
+        camera->look_at(e.get_dx(), -e.get_dy(), m_mouse_sens * dt);
+        input_handled = true;
+      }
+
+      return input_handled;
     }
 
    private:
     float m_keyboard_sens = 5.f;
+    float m_mouse_sens    = 4.f;
   };
 } // namespace sym
 
